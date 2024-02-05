da4d96c713c538f1e701b53d7ccbcf003793d8b9
Darshil Patel <117679837+LED-0102@users.noreply.github.com>
2024-02-05 09:49:53 +0530

    Update q3.cpp

--- Lab-4/q3.cpp
+++ Lab-4/q3.cpp
@@ -1,57 +1,50 @@
-#include <bits/stdc++.h>
+// USED THE CONCEPT OF QUICK SORT BUT NOT SORT THE ARRAY HERE
+// TIME COMPLEXITY :- O(N); Avarage complexity ,O(N*N) Worst Case
+// SPACE COMPLEXITY :- O(1);
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int partition(vector<int> &a, int l , int r){
-    int lst = a[r], p = l;
-    for(int i = l; i <= r; i++){
-        if(a[i] < lst)
-            swap(a[i], a[p++]);
-    }
-    swap(a[r], a[p]);
-    return p;
+int partitions(int l,int r,int a[]){
+    int x=a[r];
+    int i= l;
+	for(int j= l;j<r;j++)
+	{
+		if (a[j]<=x)
+		{
+			swap(a[i],a[j]);
+			i++;
+		}
+	}
+	swap(a[i],a[r]);
+	return i;
 }
 
-int random_partition(vector<int> &a, int l, int r){
-    int n = r - l + 1;
-    int p = (rand() % n);
-    swap(a[l + p], a[r]);
-    return partition(a, l, r);
-}
-
-void median(vector<int> &a, int l, int r, int k, int &md1, int &md2){
-    if(l > r)
-        return;
-    int par = random_partition(a, l, r);
-    if(par == k){
-        md2 = a[par];
-        if(md1 != -1)
-            return;
-    }
-    else if(par == k-1){
-        md1 = a[par];
-        if(md2 != -1)
-            return;
-    }
-    if(par >= k)
-        median(a, l, par-1, k, md1, md2);
-    else
-        median(a, par+1, r, k, md1, md2);
-}
-
-void find_median(vector<int> &a, int k){
-    int md1 = -1, md2 = -1, n = a.size();
-    median(a, 0, n-1, n-k-1, md1, md2);
-    cout<<md1<<" "<<md2<<endl;
+int Kth_element(int l,int r,int k,int a[]){
+	int pivotIdx=partitions(l,r,a);
+	if(pivotIdx-l==k-1)
+		return pivotIdx;
+	else if(pivotIdx-l>k-1)
+		return Kth_element(l,pivotIdx-1,k,a);
+	return Kth_element(pivotIdx+1,r,k-pivotIdx+l-1,a);
 }
 
 int main(){
-    freopen("inputq3.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+   freopen("inputq3.txt", "r", stdin);
+   int n;
+   int k;
+   cout<<"Enter the size of array"<<endl;
+   cin>>n;
+   cout<<"Enter the value of K"<<endl;
+   cin>>k;
+   int a[n];
+   cout<<"Enter elements array"<<endl;
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+   int z = Kth_element(0,n-1,n-k+1,a); // function call
 
-    int n; cin >> n;
-    vector<int> a(n);
-    int k; cin>>k;
-    for(int i=0; i<n; i++)
-        cin >> a[i];
-    find_median(a, k);
+   cout<<a[z]<<endl;
+   cout<<endl;
+    return 0;
 }
