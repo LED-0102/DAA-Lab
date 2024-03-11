4b7deca4a599058947c418b6deae66eb0895eae6
LED-0102 <darshilamit@gmail.com>
2024-03-11 11:00:16 +0530

    Lab 6

--- Lab-6/q2.cpp
+++ Lab-6/q2.cpp
@@ -1,36 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-int dfs(int vertex, vector<int> &vis, vector<int> adj[], vector<int> &length){
-    if (vis[vertex]!=-1){
-        return vis[vertex];
+vector<int> topologicalSort(vector<int>adj[],vector<int>&indeg,int n){
+    queue<int>q;
+    for(int i=1;i<=n;i++){
+        if(indeg[i]==0){
+            q.push(i);
+        }
     }
-    int ans=0;
-    for (int child: adj[vertex]){
-        ans = max(ans, 1+dfs(child, vis, adj, length));
+    vector<int>topoSort;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topoSort.push_back(node);
+        for(auto it:adj[node]){
+            indeg[it]--;
+            if(indeg[it]==0){
+                q.push(it);
+            }
+        }
     }
-    return vis[vertex]=ans;
+    return topoSort;
 }
+
 int main(){
-    freopen("inputq1.txt", "r", stdin);
-    int n, m;
-    cin>>n>>m;
-    vector<int> adj[n+1];
-    vector<int> length(n+1, 0);
-    vector<int> vis (n+1, -1);
-    bool possible=true;
-    for (int i=0; i<m; i++){
-        int x, y; cin>>x>>y;
+    int n;
+    cin>>n;
+    int e;
+    cin>>e;
+    vector<int>adj[n+1];
+    vector<int>indeg(n+1,0);
+    for(int i =0;i<e;i++){
+        int x,y;
+        cin>>x>>y;
         adj[x].push_back(y);
+        indeg[y]++;
     }
-    for (int i=1; i<=n; i++){
-        dfs(i, vis, adj, length);
+    vector<int>topoSort = topologicalSort(adj,indeg,n);
+    vector<int>longestPath(n,0);
+    int ans = 0;
+    for(int i=n-1;i>=0;i--){
+        for(auto it:adj[topoSort[i]]){
+           longestPath[topoSort[i]] = max(longestPath[topoSort[i]],1+longestPath[it]);
+        }
+        ans = max(ans,longestPath[topoSort[i]]);
     }
-    int mx=-1;
-    for (int i=1; i<=n;i++){
-        mx = max(mx, vis[i]);
-    }
-    cout<<mx<<endl;
-    
+    cout<<ans<<endl;
     return 0;
 }
--- Lab-6/q3.cpp
+++ Lab-6/q3.cpp
@@ -1,31 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-void dfs(int vertex, vector<int> &vis){
-    if (vis[vertex]){
-        return;
-    } else {
-        cout<<vertex<<" ";
+void dfs(int i,vector<int>&vis,vector<int>adj[],stack<int>&st){
+    vis[i] =1;
+    for(auto it:adj[i]){
+        if(vis[it]==0){
+            dfs(it,vis,adj,st);
+        }
     }
-    for (int child: g[vertex]){
-        if (child == par) continue;
-        dfs(child, vertex);
+    st.push(i);
+}
+void dfs2(int i,vector<int>&vis,vector<int>adjT[],vector<int>&temp){
+    vis[i] =1;
+    temp.push_back(i);
+    for(auto it:adjT[i]){
+        if(vis[it]==0){
+            dfs2(it,vis,adjT,temp);
+        }
     }
 }
 int main(){
-    freopen("inputq1.txt", "r", stdin);
-    int n, m;
-    cin>>n>>m;
-    vector<int> adj[n+1];
-    vector<int> vis (n+1, 0);
-    bool possible=true;
-    vector<vector<int>> cc;
-    for (int i=0; i<m; i++){
-        int x, y; cin>>x>>y;
+    int n;
+    cin>>n;
+    int e;
+    cin>>e;
+    vector<int>adj[n+1];
+    for(int i =0;i<e;i++){
+        int x,y;
+        cin>>x>>y;
         adj[x].push_back(y);
     }
-    for (int i=1; i<n; i++){
-        dfs()
+    stack<int>st; 
+    vector<int>vis(n+1,0);
+    for(int i=1;i<=n;i++){
+      if(vis[i]==0){
+        dfs(i,vis,adj,st);
+      }
     }
+    vector<int>adjT[n+1];
+    for(int i=1;i<=n;i++){
+        vis[i] =0; 
+        for(auto it:adj[i]){
+            adjT[it].push_back(i);
+        }
+    }
+    vector<vector<int>>ans;
+    while(!st.empty()){
+        int node = st.top();
+        st.pop();
+        if(vis[node]==0){
+            vector<int>temp;
+            dfs2(node,vis,adjT,temp);
+            ans.push_back(temp);
+        }
+    }
+    for(auto it:ans){
+        for(int i=0;i<it.size();i++){
+            cout<<it[i]<<" ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
