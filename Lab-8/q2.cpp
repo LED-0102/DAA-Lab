dba2ed1d2d5785315b52e8b36ddbf0a0346a33e3
LED-0102 <darshilamit@gmail.com>
2024-04-08 14:52:53 +0530

    Lab-8

--- Lab-8/q2.cpp
+++ Lab-8/q2.cpp
@@ -1,84 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 
-class DisjointSetUnion {
-    private:
-        static const int dsu_N = 1e5 + 10;
-        int parent[dsu_N];  //Stores parent node of the connected components
-        int sz[dsu_N];      //Stores size of connected components
-        int max_size;       //Shows the maximum component size
-        int conn_comp;      //Shows the number of connected components
+int graph[18][18][2];
+long long dp[1 << 18][18][2];
+long long minCost(int n, int m, int mask, int prev, int col)
+{
+    // Base case
+    if (mask == ((1 << n) - 1)) {
+        return 0;
+    }
+    // If already calculated
+    if (dp[mask][prev][col == 1] != 0) {
+        return dp[mask][prev][col == 1];
+    }
 
-    public:
-        int find(int v) {
-            if (v == parent[v]) return v;
-            return parent[v] = find(parent[v]);
-        }
-        DisjointSetUnion() {
-            max_size=0;
-            conn_comp=0;
-        }
+    long long ans = 1e9;
 
-        void make(int v) {
-            parent[v] = v;
-            sz[v] = 1;
-            conn_comp++;
-            max_size = max(max_size, 1);
-        }
-
-        void Union(int a, int b) {
-            a = find(a);
-            b = find(b);
-            if (a != b) {
-                if (sz[a] < sz[b]) {
-                    std::swap(a, b);
-                }
-                parent[b] = a;
-                sz[a] += sz[b];
-                max_size = max(max_size, sz[a]);
-                conn_comp--;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (graph[prev][i][j] && !(mask & (1 << i)) 
+            && (j != col)) {
+            long long z = graph[prev][i][j] + 
+                        minCost(n,m,mask|(1<<i),i,j);
+            ans = min(z, ans);
             }
         }
-        int get_max_size(){
-            return max_size;
-        }
-        int get_conn_comp() {
-            return conn_comp;
-        }
-};
+    }
 
-int main(){
-    int n, m; cin>>n>>m;
-    vector<pair<int, pair<int, int>>> edges;
-    map<pair<int, int>, int> present;
-    DisjointSetUnion dsu;
-    for (int i=0; i<m; i++){
-        int a, b, c, d; //d is the boolean which says the edge is between different colors (1) or between same colors (0)
-        cin>>a>>b>>c>>d;
-        if (d) edges.push_back({c, {a, b}});
-    }
-    sort(edges.begin(), edges.end());
-    for (int i=1; i<=n; i++) {
-        dsu.make(i);
-    }
-    int total_cost=0;
-    for (auto &edge: edges) {
-        int wt = edge.first;
-        int u = edge.second.first;
-        int v = edge.second.second;
+    return dp[mask][prev][col == 1] = ans;
+}
 
-        if (dsu.find(u) == dsu.find(v)) continue;
-        dsu.Union(u, v);
-        total_cost += wt;
-        present[{u, v}]=1;
+void makeGraph(vector<pair<pair<int,int>, pair<int,char>>>& vp,int m){
+
+    for (int i = 0; i < m; i++) {
+        int a = vp[i].first.first - 1;
+        int b = vp[i].first.second - 1;
+        int cost = vp[i].second.first;
+        char color = vp[i].second.second;
+        graph[a][b][color == 'W'] = cost;
+        graph[b][a][color == 'W'] = cost;
     }
-    int comp_one = dsu.find(1);
-    for (int i=2; i<=n; i++) {
-        if (comp_one != dsu.find(i)) {
-            cout<<"IMPOSSIBLE"<<endl;
-            break;
-        }
+}
+int getCost(int n,int m){
+    long long ans = 1e9;
+
+    for (int i = 0; i < n; i++) {
+        ans = min(ans, minCost(n, m, 1 << i, i, 2));
     }
+
+    if (ans != 1e9) {
+        return ans;
+    }
+    else {
+        return -1;
+    }
+}
+
+int main()
+{
+    int n = 3, m = 4;
+    vector<pair<pair<int, int>, pair<int, char> > > vp = {
+    { { 1, 2 }, { 2, 'B' } },
+    { { 1, 2 }, { 3, 'W' } },
+    { { 2, 3 }, { 4, 'W' } },
+    { { 2, 3 }, { 5, 'B' } }
+    };
+
+    makeGraph(vp,m);
+    cout << getCost(n,m) << '\n';
+
     return 0;
 }
